82dc2c90a9259faf8691ae99598d7218975d1869
Otisium <kartikmhatre1812@gmail.com>
2025-10-14 18:46:34 +0530

    Update s3.ts

--- frontend/src/lib/s3.ts
+++ frontend/src/lib/s3.ts
@@ -1,4 +1,4 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+ import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { env } from "~/env";
 
